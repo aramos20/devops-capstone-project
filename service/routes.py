a79e4dde92d293a6a67f3c08efe786bba9ecb59a
rwby <56618279+aramos20@users.noreply.github.com>
2024-08-20 03:04:55 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -56,7 +56,7 @@ def create_accounts():
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
-    
+
 
 ######################################################################
 # LIST ALL ACCOUNTS
@@ -90,7 +90,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
-    
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -108,7 +108,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
-    
+
 
 ######################################################################
 # DELETE AN ACCOUNT
